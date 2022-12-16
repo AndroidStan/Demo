bb54a472531fb683e8d6d01285778f7af733f2ce
AndroidStan <stan_dinev@abv.bg>
2022-12-16 12:01:21 +0200

    Add files via upload

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,5 +1,1 @@
-CREATE TABLE IF NOT EXISTS "customer"  (
-  "id" VARCHAR(36) PRIMARY KEY,
-  "firstname" VARCHAR(512),
-  "lastname" VARCHAR(512)
-);
+CREATE TABLE IF NOT EXISTS customer ( "id" VARCHAR(36) PRIMARY KEY, "firstname" VARCHAR(512), "lastname" VARCHAR(512) );
